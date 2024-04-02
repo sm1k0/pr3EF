a0b5c1332311817f018d3b4df70b9558eb462a22
sm1k0 <mastermode1234567890@gmail.com>
2024-04-02 12:02:49 +0300

    Добавьте файлы проекта.

---
+++ pr3EF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr3EF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private prakta3Entities1 context = new prakta3Entities1();
+        public MainWindow()
+        {
+            InitializeComponent();
+            OrdersDrd.ItemsSource = context.Projects.ToList();
+            
+        }
+
+       
+    }
+}
